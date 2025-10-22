08cd82d0ed1338e6ad2f878d097da32dd50ef5fc
Mostafa Mirmousavi <mirmousavi.m@gmail.com>
2025-10-22 23:55:35 +0200

    ci: update GH action for base path

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   trailingSlash: true,
-  basePath: "/mostage-editor",
-  assetPrefix: "/mostage-editor",
   images: {
     unoptimized: true,
   },
